623984d03a56c757bc8c16590a4b4f83cfa7da06
liqiang <liqiang>
2021-01-08 21:27:46 +0800

    add multi_task_3

--- my_model/MultiTask.py
+++ my_model/MultiTask.py
@@ -43,3 +43,24 @@ class Multi_Task_2(nn.Module) :
         vis_ditance = self.task2(m_x, fog_level)
 
         return fog_level, vis_ditance
+
+class Multi_Task_3(nn.Module) :
+    def __init__(self):
+        super(Multi_Task_3, self).__init__()
+        self.softmax = nn.Softmax(dim=1)
+        self.g_feature = g_exactor(in_chanel=4, out_chanel=1024)
+        self.d_feature = g_exactor(in_chanel=3, out_chanel=272)
+        self.task1 = FogLevel_Classify(input_channel=1)
+        self.task2 = VisDistance_Estimation(input_channel=1)
+
+    def forward(self, x1, x2):
+        g_x = self.g_feature(x1)
+        d_x = self.d_feature(x2)
+
+        m_x = torch.cat((d_x, g_x), dim=1)
+        g_x = torch.reshape(m_x, (m_x.size(0), 1, 36, 36))
+
+        fog_level = self.task1(m_x)
+        vis_ditance = self.task2(m_x, fog_level)
+
+        return fog_level, vis_ditance
